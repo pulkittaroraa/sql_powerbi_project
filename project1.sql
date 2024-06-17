01eb3537162ba70ac9afbd044d89088d5aaf8886
pulkittaroraa <pulkit.arora907@gmail.com>
2024-06-17 16:28:13 +0530

    Add files via upload

---
+++ project1.sql
@@ -0,0 +1,19 @@
+WITH cte AS(
+SELECT * FROM bike_share_yr_0
+union
+SELECT * FROM bike_share_yr_1)
+
+SELECT 
+dteday, 
+season, 
+a.yr, 
+weekday, 
+hr, 
+rider_type, 
+riders, 
+price, 
+COGS, 
+riders * price AS revenue,
+riders * price - COGS AS profit
+FROM cte a
+LEFT JOIN cost_table b ON a.yr = b.yr;
